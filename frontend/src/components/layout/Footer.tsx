02dd03834d314b3453afcc9433618780f7c016c0
deltaSticker <yttrium@tuta.io>
2025-12-24 02:44:24 -0300

    feat: added footer (fr this time)

---
+++ frontend/src/components/layout/Footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+
+    return (
+        <footer className="h-80 bg-neutral-900 text-white w-full items-center flex justify-center">
+            <p><i>Contenido</i></p>
+        </footer>
+    )
+}
