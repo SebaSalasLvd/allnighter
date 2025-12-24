6429806469231efa1f105cec5c928b8ef8ea0b9f
deltaSticker <yttrium@tuta.io>
2025-12-24 02:56:26 -0300

    refactor(component): added icons to navbar

--- frontend/src/components/layout/Navbar.tsx
+++ frontend/src/components/layout/Navbar.tsx
@@ -1,21 +1,20 @@
-
-
+import { Shirt, type LucideIcon } from "lucide-react"
 
 export default function NavBar() {
     const items = [
-        { name: 'Tienda', href: '#' },
-        { name: 'Tienda', href: '#' },
-        { name: 'Tienda', href: '#' },
-        { name: 'Tienda', href: '#' },
-        { name: 'Tieada', href: '#' },
+        {icon: Shirt, name: 'Tienda', href: '#' },
+        {icon: Shirt, name: 'Tienda', href: '#' },
+        {icon: Shirt, name: 'Tienda', href: '#' },
+        {icon: Shirt, name: 'Tienda', href: '#' },
+        {icon: Shirt, name: 'Tieada', href: '#' },
     ]
 
     return (
-        <nav className="flex h-8 px-4 shadow-xl w-full bg-neutral-800 text-white sticky top-0">
+        <nav className="flex h-8 px-4 shadow-xl w-full bg-neutral-900 text-white sticky top-0">
             <h1 className="text-xl">AllNighter</h1>
             <ul className="flex justify-center w-full">
-                {items.map((item, i) => (
-                    <NavItem name={item.name} href={item.href} />
+                {items.map((item) => (
+                    <NavItem icon={item.icon} name={item.name} href={item.href} />
                 ))}
             </ul>
         </nav>
@@ -23,6 +22,13 @@ export default function NavBar() {
 }
 
 
-function NavItem(props: { name: string, href: string }) {
-    return <li className="px-6 font-bold"><a href={props.href}>{props.name}</a></li>
+function NavItem(props: { icon: LucideIcon, name: string, href: string }) {
+    return (
+        <li className="px-6 font-bold flex items-center">
+            <props.icon size={17} className=""/>
+            <a href={props.href} className="px-2">
+                {props.name}
+            </a>
+        </li>
+    )
 }
